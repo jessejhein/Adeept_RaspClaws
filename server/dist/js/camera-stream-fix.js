f3406d7464ef60071fe6c7b5566d56443a7c92fc
Jesse Hein <jessejhein@users.noreply.github.com>
2026-07-19 13:48:57 -0500

    Polish robot web UI

---
+++ server/dist/js/camera-stream-fix.js
@@ -0,0 +1,44 @@
+(function () {
+  function streamUrl() {
+    return window.location.protocol + "//" + window.location.hostname + ":5000/video_feed?rand=" + Date.now();
+  }
+
+  function replaceCanvas(canvas) {
+    if (!canvas) {
+      return false;
+    }
+
+    if (canvas.dataset.mjpegFixed === "1") {
+      return true;
+    }
+
+    var img = document.createElement("img");
+    img.className = canvas.className + " mjpeg-stream";
+    img.alt = "Camera stream";
+    img.src = streamUrl();
+    img.style.display = "block";
+    img.style.width = "100%";
+    img.style.height = "auto";
+
+    canvas.dataset.mjpegFixed = "1";
+    canvas.parentNode.replaceChild(img, canvas);
+    return true;
+  }
+
+  function install() {
+    var attempts = 0;
+    var timer = window.setInterval(function () {
+      attempts += 1;
+      var canvas = document.querySelector(".vedio-wrapper canvas");
+      if (replaceCanvas(canvas) || attempts > 100) {
+        window.clearInterval(timer);
+      }
+    }, 100);
+  }
+
+  if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", install);
+  } else {
+    install();
+  }
+}());
